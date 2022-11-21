1c8a7b6dd8c2455308284a56f4378ee4d2dafe57
Robert Chung 1 <bobgrunge@gmail.com>
2022-11-20 23:28:34 -0500

    had to update the image sources due to 403s from FB

--- components/HomeScreen/components/Stories/Stories.js
+++ components/HomeScreen/components/Stories/Stories.js
@@ -6,16 +6,26 @@ export const Stories = () => {
         <View style={{ marginBottom: 13 }}>
             <ScrollView horizontal showsHorizontalScrollIndicator={false}>
                 {USERS.map((user, index) => (
-                    <Image source={{uri: user.image}} 
-                    style={styles.story}/>
+                    <View key={index} style={{alignItems: 'center'}}>
+                        <Image source={{uri: user.image}} style={styles.story}/>
+                        <Text style={{color: 'white'}}>{
+                        user.user.length > 11 ? user.user.slice(0, 9) + '...' : user.user
+                        }</Text>
+                    </View>
                 ))}
             </ScrollView>
+            <Text style={{ color: 'white'}}>Stories</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
    story: {
-    
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+    marginLeft: 6,
+    borderWidth: 3,
+    borderColor: '#ff8501',
    }
 });
